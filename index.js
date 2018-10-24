27e4353ce83a06b6668c6c817812ba68fd1825ee
rchipka <rchipka@vibethink.com>
2018-10-24 15:33:34 -0400

    v0.3.11

--- index.js
+++ index.js
@@ -321,8 +321,8 @@ module.exports = function (opts) {
             .replace(/&amp;lt;\?php\s.+?\s\?&amp;gt;\s*($|<)/g, function (str) {
               return entities.decodeHTML(entities.decodeHTML(str));
             })
-            .replace(/&lt;?php.+?\s\?&gt;/g, function (str) {
-              return decodeURIComponent(str);
+            .replace(/&lt;\?php.+?\?&gt;/g, function (str) {
+              return decodeURIComponent(entities.decodeHTML(str));
             });
       // console.log(c.html);
       // delete c.node;
